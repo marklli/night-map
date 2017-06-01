289b2fb464184c88c75ce3c201bd8352aa622948
Benjamin Earl <benearl72@gmail.com>
2017-06-01 11:34:31 -0600

    Version 2!

--- main.js
+++ main.js
@@ -1,20 +1,17 @@
-module.exports = function (nightmare, array, iteri, callback) {
-	var arr = []
-	function next(index) {
-		if (index < array.length) {
-			return iteri(nightmare, array[index])
-				.then(val => {
-					arr.push(val)
-					return next(index + 1)
-				})
-				.catch(err => {
-					callback(err)
-				})
-		} else {
-			callback(null,arr)
-		}
-	}
-	nightmare.then(() => {
-		return next(0)
+module.exports = function(Nightmare){
+	Nightmare.action('map', function (iteri, arr, done) {
+		// Thanks to @rosshinkley 's code for the Promise.resolve([]) and way of passing the results around
+		// https://github.com/rosshinkley/nightmare-examples/blob/master/docs/common-pitfalls/async-operations-loops.md
+		arr.reduce((acm, elm) =>
+			acm.then(results =>
+				iteri(elm).then(result => {
+					results.push(result);
+					return results;
+				})), Promise.resolve([])).then(results => {
+			this.evaluate_now(function (results) {
+				return results
+			}, done, results)
+		})
 	})
+	return Nightmare
 }
