010a2f7f055930486d8b52a82af861fdca9a9e0a
Marcos Lima <marcos@enesolucoes.com.br>
2017-12-06 18:23:11 -0200

    Get index array

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ module.exports = function(Nightmare){
 	Nightmare.action('map', function (iteri, arr, done) {
 		// Thanks to @rosshinkley 's code for the Promise.resolve([]) and way of passing the results around
 		// https://github.com/rosshinkley/nightmare-examples/blob/master/docs/common-pitfalls/async-operations-loops.md
-		arr.reduce((acm, elm) =>
+		arr.reduce((acm, elm, index) =>
 			acm.then(results =>
 				iteri(elm).then(result => {
-					results.push(result);
+					results.push({ index, result });
 					return results;
 				})), Promise.resolve([])).then(results => {
 			this.evaluate_now(function (results) {
